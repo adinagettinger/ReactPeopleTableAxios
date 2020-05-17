197ecc4285e97f4c63e00a071c577822e59e8812
Adina <adinagettinger97@gmail.com>
2020-05-17 16:42:28 -0400

    Add project files.

---
+++ ReactPeopleTableAxios.Data/PeopleContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ReactPeopleTableAxios.Data
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public int id { get; set; }
+    }
+
+    public class PeopleContext
+    {
+        private string _connectionString;
+
+        public PeopleContext(string connection)
+        {
+            _connectionString = connection;
+        }
+
+        public DbSet<Person> people { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(_connectionString);
+        }
+    }
+
+    public class PeopleRepository
+    {
+
+    }
+}
---
+++ ReactPeopleTableAxios.Data/PeopleContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.IO;
+
+namespace ReactPeopleTableAxios.Data
+{
+    public class PeopleContextFactory : IDesignTimeDbContextFactory<PeopleContext>
+    {
+        public PeopleContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), $"..{Path.DirectorySeparatorChar}ReactPeopleTableAxios.Web"))
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true).Build();
+
+            return new PeopleContext(config.GetConnectionString("ConStr"));
+        }
+    }
+}
